325beb6b3bf8d1b57ba307d1049ff9a39f7f6ab0
mohamed-jalel-amara <amarajalel11@gmail.com>
2023-05-24 23:17:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv").config(); 
+const city = "sousse";
+
+
+
+const axios = require("axios").default;
+axios({
+	method: "get",
+	url:
+		`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.API_KEY}&units=metric&q=` +
+		city,
+})
+	.then((res) => {
+		console.log("\nUsing AXIOS : \n");
+		console.log("City: " + res.data.name);
+		console.log("Weather: " + res.data.weather[0].main);
+		console.log("Weather description: " + res.data.weather[0].description);
+		console.log("Temperature: " + res.data.main.temp);
+		console.log("Humidity: " + res.data.main.humidity);
+		console.log("Wind Speed: " + res.data.wind.speed);
+	})
+	.catch((err) => console.log(err));
+
+
+
+
+const request = require("request");
+request(
+	`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.API_KEY}&units=metric&q=` +
+		city,
+	function (error, response, body) {
+		if (error) {
+			console.error("error:", error);
+			console.log("statusCode:", response && response.statusCode);
+		}
+
+		const weatherData = JSON.parse(body);
+		console.log("\nUsing request : \n");
+		console.log("City: " + weatherData.name);
+		console.log("Weather: " + weatherData.weather[0].main);
+		console.log("Weather description: " + weatherData.weather[0].description);
+		console.log("Temperature: " + weatherData.main.temp);
+		console.log("Humidity: " + weatherData.main.humidity);
+		console.log("Wind Speed: " + weatherData.wind.speed);
+	}
+);
